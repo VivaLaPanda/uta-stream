99718992a4f409d347ad9f9da7fe38b845d8aaaa
Adrian Reed Smith <adrian@smithdev.io>
2018-10-03 19:46:49 -0700

    Writing out q testcases, fixing bugs

--- queue/queue.go
+++ queue/queue.go
@@ -7,17 +7,18 @@ import (
 type Queue struct {
 	fifo         []string
 	autoq        *auto.Queue
-	autoqEnabled bool
+	AutoqEnabled bool
 }
 
-func NewQueue() *Queue {
-	return &Queue{autoq: auto.NewQueue("autoq.db")}
+// Make a new q structure
+func NewQueue(autoqFilename string, enableAutoq bool) *Queue {
+	return &Queue{autoq: auto.NewQueue(autoqFilename), AutoqEnabled: enableAutoq}
 }
 
 // Returns the audio resource next in the queue
 func (q *Queue) Pop() (filename string, emptyq bool) {
 	if len(q.fifo) == 0 {
-		if q.autoqEnabled {
+		if q.AutoqEnabled {
 			return q.autoq.Vpop(), false
 		} else {
 			return "", true
