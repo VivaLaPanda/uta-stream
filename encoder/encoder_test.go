e45a333a10a8b05f14c4784c2614bb9e511477f2
Adrian Smith <adrian.smith@keysight.com>
2018-10-16 15:29:51 -0700

    rewrite of encoder to calc bitrate in parallel

--- encoder/encoder_test.go
+++ encoder/encoder_test.go
@@ -9,11 +9,16 @@ func TestEncodeMP3(t *testing.T) {
 	filename := "test_counting.mp3"
 
 	fileR, err := os.Open(filename)
-	defer fileR.Close()
+	if err != nil {
+		t.Errorf("Failed to open test file: %v", err)
+	}
 
 	// Start pulling data from file into the channel
-	_, err = EncodeMP3(fileR, 2)
+	testChan, err := EncodeMP3(fileR, 2)
 	if err != nil {
 		t.Errorf("Failed to generate data stream: %v", err)
+	}
+	for val := range *testChan {
+		_ = val
 	}
 }
