be23640dadc19609ab249692a8f0b980316d3408
Adrian Smith <adrian.smith@keysight.com>
2018-10-16 14:29:35 -0700

    dl/convert now happens in parallel via io streams

--- resource/download/download_test.go
+++ resource/download/download_test.go
@@ -1,17 +1,39 @@
 package download
 
 import (
+	"io"
 	"net/url"
+	"os"
 	"testing"
 
 	shell "github.com/ipfs/go-ipfs-api"
 )
 
 func TestSplitAudio(t *testing.T) {
-	_, err := splitAudio("test_vid", false)
+	inputFile, _ := os.Open("test_vid.mp4")
+	outputFile, _ := os.Create("test_audio.mp3.test")
+	convInput, convOutput, convProgress, err := splitAudio()
 	if err != nil {
 		t.Errorf("testsplitaudio failed due to an error: %v", err)
 	}
+
+	go func() {
+		_, err := io.Copy(convInput, inputFile)
+		if err != nil {
+			t.Errorf("failed to copy audio from input into converter: %v", err)
+		}
+		convInput.Close()
+		inputFile.Close()
+	}()
+	go func() {
+		_, err := io.Copy(outputFile, convOutput)
+		if err != nil {
+			t.Errorf("failed to copy audio from converter into output: %v", err)
+		}
+		convOutput.Close()
+		outputFile.Close()
+	}()
+	convProgress.Wait()
 }
 
 func TestDownloadYoutube(t *testing.T) {
