81b9642e6695eaa61807721e892cd90ec414f4fe
Adrian Smith <adrian.smith@keysight.com>
2018-11-27 15:52:47 -0800

    undoing changes to mixer autoq notify

--- mixer/mixer.go
+++ mixer/mixer.go
@@ -82,7 +82,9 @@ func NewMixer(queue *queue.Queue, bitrate int) *Mixer {
 				// We couldn't play from current, assume that the song ended
 				// Also, if we just recieved a skip, then we don't want to use that
 				// song to train qutoq
-				mixer.queue.NotifyDone(mixer.CurrentSongInfo.IpfsPath(), mixer.learnFrom)
+				if mixer.CurrentSongInfo.IpfsPath() != "" {
+					mixer.queue.NotifyDone(mixer.CurrentSongInfo.IpfsPath(), mixer.learnFrom)
+				}
 				mixer.learnFrom = true
 			}
 		}
